fea70c70f9eba6c43d5e87a6c9d8355b476126c9
H. Joe LEE <hyoklee@hdfgroup.org>
2022-08-02 21:36:20 -0400

    DLIB-28: Run 1 fput and 1 fget multi-node test.

--- scripts/w_h.sh
+++ scripts/w_h.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Write hostname to a file.
 FILE=/ccs/proj/csc332/fget_host.txt
 if test -f "$FILE"; then
     echo "$FILE exists. Running fput."
-    cat /proc/sys/kernel/hostname
+    /ccs/proj/csc332/usr/local/transfer/fput `cat /ccs/proj/csc332/fget_host.txt`
 else
     echo "Running fget."
     cat /proc/sys/kernel/hostname > $FILE
-    ls /ccs/proj/csc332
+    ls  -l /ccs/proj/csc332/fget_host.txt
+    /ccs/proj/csc332/usr/local/transfer/fget -b `cat /proc/sys/kernel/hostname`
 fi
