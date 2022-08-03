8e800b7c773e20271f5740146163ad610f142ead
H. Joe LEE <hyoklee@hdfgroup.org>
2022-08-02 22:15:21 -0400

    DLIB-28: Run 1 fget and 2 fputs.

--- scripts/w_h.sh
+++ scripts/w_h.sh
@@ -3,10 +3,11 @@
 FILE=/ccs/proj/csc332/fget_host.txt
 if test -f "$FILE"; then
     echo "$FILE exists. Running fput."
-    /ccs/proj/csc332/usr/local/transfer/fput `cat /ccs/proj/csc332/fget_host.txt`
+    # /ccs/proj/csc332/usr/local/transfer/fput `cat /ccs/proj/csc332/fget_host.txt`
+    /ccs/proj/csc332/usr/local/transfer/fput -n 4 -k 2 `cat /ccs/proj/csc332/fget_host.txt`
 else
     echo "Running fget."
     cat /proc/sys/kernel/hostname > $FILE
     ls  -l /ccs/proj/csc332/fget_host.txt
-    /ccs/proj/csc332/usr/local/transfer/fget -b `cat /proc/sys/kernel/hostname`
+    /ccs/proj/csc332/usr/local/transfer/fget -n 4 -b `cat /proc/sys/kernel/hostname`
 fi
