180eadf5a5e2f058c5f152c8ae68911580777e73
H. Joe LEE <hyoklee@hdfgroup.org>
2022-08-05 12:24:30 -0400

    DLIB-28: Collect initial data.

--- scripts/fget2.sh
+++ scripts/fget2.sh
@@ -3,4 +3,4 @@ echo "Running fget."
 FILE=/ccs/proj/csc332/fget_host2.txt
 cat /proc/sys/kernel/hostname > $FILE
 cat /ccs/proj/csc332/fget_host2.txt
-/ccs/proj/csc332/usr/local/transfer/fget -n 4 -b `cat /proc/sys/kernel/hostname`
+time -p /ccs/proj/csc332/usr/local/transfer/fget -n 4 -b `cat /proc/sys/kernel/hostname`
