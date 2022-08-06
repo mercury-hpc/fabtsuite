e9b8fda8863129a074a369b804a5a9c93736623f
H. Joe LEE <hyoklee@hdfgroup.org>
2022-08-05 22:38:36 -0400

    DLIB-12: Test -r option.

--- scripts/fput2.sh
+++ scripts/fput2.sh
@@ -4,5 +4,5 @@ FILE=/ccs/proj/csc332/fget_host2.txt
 HOST=`cat /proc/sys/kernel/hostname`
 if test -f "$FILE"; then
     echo "$FILE exists. Running fput."
-    { time -p /ccs/proj/csc332/usr/local/transfer/fput -n 4 -k 2 `cat /ccs/proj/csc332/fget_host2.txt`; }&> /ccs/proj/csc332/$HOST.txt
+    { time -p /ccs/proj/csc332/usr/local/transfer/fput -r -n 4 -k 2 `cat /ccs/proj/csc332/fget_host2.txt`; }&> /ccs/proj/csc332/$HOST.txt
 fi
