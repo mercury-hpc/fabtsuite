924adf1d921fda1110916a5dab484bb58ff18b66
H. Joe LEE <hyoklee@hdfgroup.org>
2022-08-30 22:23:54 -0400

    DLIB-40: Update file path.

--- scripts/fabtget2.sh
+++ scripts/fabtget2.sh
@@ -3,4 +3,4 @@ echo "Running fabtget."
 FILE=/ccs/proj/csc332/fabtget_host2.txt
 cat /proc/sys/kernel/hostname > $FILE
 cat /ccs/proj/csc332/fabtget_host2.txt
-time -p /ccs/proj/csc332/usr/local/transfer/fabtget -n 4 -b `cat /proc/sys/kernel/hostname`
+time -p /ccs/proj/csc332/usr/local/bin/fabtget -n 4 -b `cat /proc/sys/kernel/hostname`
--- scripts/fabtput2.sh
+++ scripts/fabtput2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sleep 2
-FILE=/ccs/proj/csc332/fget_host2.txt
+FILE=/ccs/proj/csc332/fabtget_host2.txt
 HOST=`cat /proc/sys/kernel/hostname`
 if test -f "$FILE"; then
     echo "$FILE exists. Running fabtput."
-    { time -p /ccs/proj/csc332/usr/local/bin/fabtput -g -n 4 -k 2 `cat /ccs/proj/csc332/fget_host2.txt`; }&> /ccs/proj/csc332/$HOST.txt
+    { time -p /ccs/proj/csc332/usr/local/bin/fabtput -g -n 4 -k 2 `cat /ccs/proj/csc332/fabtget_host2.txt`; }&> /ccs/proj/csc332/$HOST.txt
 fi
