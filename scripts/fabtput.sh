0871842d841ac917e8e6039e23a726e27e756c99
H. Joe Lee <hyoklee@hdfgroup.org>
2022-09-07 02:02:11 +0000

    DLIB-40: Use Lustre file system paths.

--- scripts/fabtput.sh
+++ scripts/fabtput.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-FILE=/home/hyoklee/fabtget_a.txt
+PREFIX=/lus/grand/projects/radix-io
+
+# On Polaris, fabtget can't write to Lustre file system.
+FILE=$HOME/fabtget_a.txt
 
 # Get the host name of node to ensure that 2 nodes were used.
 HOST=`cat /proc/sys/kernel/hostname`
@@ -9,4 +12,4 @@ HOST=`cat /proc/sys/kernel/hostname`
 sleep 2
 echo "$FILE exists. Running fabtput."
 cat $FILE
-{ time -p /home/hyoklee/spack/opt/spack/cray-sles15-zen3/gcc-10.3.0/fabtsuite-main-aggqguz24qiydkvii7rzqw76nsenichb/bin/fabtput `cat /home/hyoklee/fabtget_a.txt`; } &> /home/hyoklee/$HOST.txt
+{ time -p $PREFIX/bin/fabtput `cat $FILE`; } &> $PREFIX/$HOST.txt
