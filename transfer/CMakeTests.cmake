1c5d47582d15a15eba3cddb749a3cca8761e39f2
H. Joe Lee <hyoklee@hdfgroup.org>
2022-09-12 21:40:23 +0000

    DLIB-40: Simulate a complete CTest output.

--- transfer/CMakeTests.cmake
+++ transfer/CMakeTests.cmake
@@ -1,8 +1,10 @@
+# Test local.
 add_test (
-    NAME default
+    NAME single-node
     COMMAND test.sh
 )
 
+# Test Cori.
 if (${SLURM})
   add_test (
       NAME slurm
@@ -10,9 +12,41 @@ if (${SLURM})
   )
 endif ()  
 
+# Test Polaris.
 if (${PBS})
   add_test (
-      NAME pbs
-      COMMAND qsub -W block=true fabtrun.qsub
+      NAME multi-node
+      COMMAND qsub -W block=true fabtrun.qsub # default
   )
+  
+  add_test (
+      NAME FI_WAIT_FD
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
+  add_test (
+      NAME fi_cancel
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
+  add_test (
+      NAME cross-job-comm
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
+  add_test (
+      NAME multi-thread
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
+  add_test (
+      NAME vectored-IO
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
+  add_test (
+      NAME MPI-interoperability
+      COMMAND qsub -W block=true fabtrun.qsub 
+  )
+
 endif ()  
