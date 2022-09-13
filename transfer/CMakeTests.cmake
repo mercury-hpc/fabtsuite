3615c3da0824e159ffd2a0151ffcefe141ba3762
H. Joe Lee <hyoklee@hdfgroup.org>
2022-09-13 01:31:10 +0000

    DLIB-40: Add tests for Cori.

--- transfer/CMakeTests.cmake
+++ transfer/CMakeTests.cmake
@@ -6,14 +6,46 @@ add_test (
 
 # Test Cori.
 if (${SLURM})
+
   add_test (
-      NAME slurm
+      NAME multi-node
       COMMAND sbatch --wait test.slurm
   )
+  add_test (
+      NAME FI_WAIT_FD
+      COMMAND sbatch --wait test.slurm
+  )
+
+  add_test (
+      NAME fi_cancel
+      COMMAND sbatch --wait test.slurm
+  )
+
+  add_test (
+      NAME cross-job-comm
+      COMMAND sbatch --wait test.slurm
+  )
+
+  add_test (
+      NAME multi-thread
+      COMMAND sbatch --wait test.slurm
+  )
+
+  add_test (
+      NAME vectored-IO
+      COMMAND sbatch --wait test.slurm
+  )
+
+  add_test (
+      NAME MPI-interoperability
+      COMMAND sbatch --wait test.slurm
+  )
+  
 endif ()  
 
 # Test Polaris.
 if (${PBS})
+
   add_test (
       NAME multi-node
       COMMAND qsub -W block=true fabtrun.qsub # default
