b703d7bf0f4eae73cca4d5a75e99b1f117f3a23a
H. Joe Lee <hyoklee@hdfgroup.org>
2022-09-15 03:36:51 +0000

    DLIB-40: Fix grammar in comment.

--- test/tput.sh
+++ test/tput.sh
@@ -8,6 +8,6 @@ fi
 HOST=`cat /proc/sys/kernel/hostname`
 if test -f "$WORKDIR/$FILE"; then
     echo "$WORKDIR/$FILE exists. Running fabtput $1"
-    { time -p $WORKDIR/fabtput `cat $WORKDIR/$FILE`; } &> $WORKDIR/$HOST.txt
+    { time -p $WORKDIR/fabtput $1 `cat $WORKDIR/$FILE`; } &> $WORKDIR/$HOST.txt
     echo "Result is written to $WORKDIR/$HOST.txt."
 fi
