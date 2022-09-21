526c3cfa53fbd866c44cfb6326a020ae3b4d804b
H. Joe LEE <hyoklee@hdfgroup.org>
2022-09-21 11:03:48 -0400

    DLIB-40: Wrap timeout for cancel test.

--- test/tget.sh
+++ test/tget.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 FILE=fabtget_address.txt
 WORKDIR=$PBS_O_WORKDIR
+
 if [ -z $WORKDIR ] ; then
     WORKDIR=.
 fi
-echo "Running $WORKDIR/fabtget $@ -a $WORKDIR/$FILE."
-time -p $WORKDIR/fabtget $@ -a $WORKDIR/$FILE
+
+# Check if cancel option is used.
+if [[ "$@" == "-c" ]]; then
+    TO="timeout --preserve-status -s INT 2 "
+else
+    TO=""
+fi
+
+echo "Running $TO $WORKDIR/fabtget $@ -a $WORKDIR/$FILE."
+$TO time -p $WORKDIR/fabtget $@ -a $WORKDIR/$FILE
+
 ret=$?
 echo $ret
 exit $ret
--- test/tput.sh
+++ test/tput.sh
@@ -8,9 +8,17 @@ fi
 HOST=`cat /proc/sys/kernel/hostname`
 DT=$(date '+%Y-%m-%dT%H:%M:%SZ');
 output=$WORKDIR/$HOST"_"$DT.txt
+
+# Check if cancel option is used.
+if [[ "$@" == "-c" ]]; then
+    TO="timeout --preserve-status -s INT 2 "
+else
+    TO=""
+fi
+
 if test -f "$WORKDIR/$FILE"; then
-    echo "$WORKDIR/$FILE exists. Running fabtput $@"
-    { time -p $WORKDIR/fabtput $@ `cat $WORKDIR/$FILE`; } &> $output
+    echo "$WORKDIR/$FILE exists. Running $TO fabtput $@"
+    { time -p $TO $WORKDIR/fabtput $@ `cat $WORKDIR/$FILE`; } &> $output
     echo "Result is written to $output."
 fi
 ret=$?
